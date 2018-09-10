50ed9981287e704ee257ea2ec90b84939ca2f367
purez <lixia1855@gmail.com>
2018-09-10 13:05:54 +0800

    update

--- src/wc/Test.java
+++ src/wc/Test.java
@@ -5,33 +5,54 @@ import java.io.*;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
-		
 		int index = 1;
 		while(args[index].length()==2)
 			index++;
 		File f = new File(args[index]);
-		
-		/*if(f.isDirectory() && args[findLocation(args) - 1].endsWith("s")) {
-			System.out.println("us '-u' for directory");   错误处理、有s才递归
-		}*/
-		
-		File[] files = f.listFiles();
 		ToolBox toolBox = new ToolBox();
-		if(args[0].equals("-c")) {
-			//System.out.println("n");
-			for(File file : files) {
-				toolBox.countChar(file.getAbsoluteFile());
+		if(f.isDirectory()) {
+			
+			FileFilter fileFilter = new FileFilter() {
+				@Override
+				public boolean accept(File arg0) {  //参数是具体文件名
+					// TODO Auto-generated method stub
+					if(arg0.getName().endsWith(".java"))
+						return true;
+					return false;
+				}
+			};
+			
+			
+			File[] files = f.listFiles(fileFilter);
+			
+			if(args[0].equals("-c")) {
+				//System.out.println("n");
+				for(File file : files) {
+					toolBox.countChar(file.getAbsoluteFile());
+				}
+			}
+			if(args[0].equals("-w")) {
+				for(File file : files) {
+					toolBox.countWord(file.getAbsoluteFile());
+				}
+			}
+			if(args[0].equals("-l")) {
+				for(File file : files) {
+					toolBox.countLine(file.getAbsoluteFile(), args[1]);
+				}
 			}
 		}
-		if(args[0].equals("-w")) {
-			for(File file : files) {
-				toolBox.countWord(file.getAbsoluteFile());
-			}
-		}
-		if(args[0].equals("-l"))
-			for(File file : files) {
-				toolBox.countLine(file.getAbsoluteFile(), args[1]);
-			}
+	  	else {
+	  		if(args[0].equals("-c")) {
+	  			toolBox.countChar(f);
+	  		}
+	  		if(args[0].equals("-w")) {
+	  			toolBox.countWord(f);
+	  		}
+	  		if(args[0].equals("-l")) {
+	  			toolBox.countLine(f, args[1]);
+	  		}
+	  	}		
 		
 	}
 	
